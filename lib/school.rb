124cb1408d70c8a712111872ee0f04f3a091f7a7
William Han Findiesen <william.findiesen@gmail.com>
2020-07-21 04:32:54 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  
+  def add_student(student, level)
+    @roster[level] ||=[]
+    @roster[level] << student
+  end
+  
+  def grade(level)
+    @roster.each do |levels, students|
+      if levels == level
+        return students
+      end
+    end
+  end
+    
+  def sort
+    sortedhash = {}
+    @roster.each do|levels, students|
+    sortedhash[levels] = students.sort
+  end
+
+  sortedhash
+end  
+end
