95687b4e991ae65babb90640bb89b698271d587e
William Han Findiesen <william.findiesen@gmail.com>
2020-07-21 04:11:40 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,5 +1,18 @@
 class School
-  attr_accessor
+  attr_accessor :roster
   
   def initialize(name)
+    @name = name
+  end
+  
+  def self.roster
+    @roster = []
+  end
+  
+  def add_student
+  end
+  
+  def grade
+  end
+  
 end
